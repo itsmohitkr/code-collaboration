fe2274578ef92a7ee6fc8d6770745acde56a4912
Mohit kumar <50844220+itsmohitkr@users.noreply.github.com>
2021-07-21 16:14:38 +0530

    spelling corrected

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ io.on('connection',(socket) => {
 
         // notification message
 
-        socket.emit("notification", "welcome 👏 to code </> collabration 💻.");
+        socket.emit("notification", "welcome 👏 to the code </> collaboration app 💻.");
         socket.broadcast.emit("notification", `A new user has joined the chat. 🤩`)
         
         socket.on("disconnect", () => {
